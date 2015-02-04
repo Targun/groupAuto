a3785b0a4519ccce24e09df7ed0c77c0bde1c574
PJ Targun <pjtargun@gmail.com>
2015-02-04 15:07:57 -0600

    Changed to typehead

---
+++ public/shared/directives/typeheadItem.directive.js
@@ -0,0 +1,30 @@
+(function() {
+	'use strict';
+	angular
+	.module('app')
+	.directive('typeaheadItem', function(){
+		return {
+            require: '^typeahead',
+            link: function(scope, element, attrs, controller) {
+
+                var item = scope.$eval(attrs.typeaheadItem);
+
+                scope.$watch(function() { return controller.isActive(item); }, function(active) {
+                    if (active) {
+                        element.addClass('active');
+                    } else {
+                        element.removeClass('active');
+                    }
+                });
+
+                element.bind('mouseenter', function(e) {
+                    scope.$apply(function() { controller.activate(item); });
+                });
+
+                element.bind('click', function(e) {
+                    scope.$apply(function() { controller.select(item); });
+                });
+            }
+        };
+	});  
+})();
